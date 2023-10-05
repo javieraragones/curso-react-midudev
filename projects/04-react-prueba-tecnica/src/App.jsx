371e1af7ae125db8b98c80f72b83d26ba0c88539
javieraragones <javieraragonesgutierrez@gmail.com>
2023-10-05 19:01:25 +0200

    Prueba técnica terminada con varias soluciones

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react"
+import './App.css'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMAGE_URL = `https://api.thecatapi.com/v1/images/search`
+
+export function App() {
+    const [fact, setFact] = useState("lorem ipsum fact whatever")
+    const [imageURL, setImageURL] = useState()
+    const [factError, setFactError] = useState()
+
+    //aprender fetch ya que a veces no puedes usar dependencias (React Query, SWR, axios, apollo)
+    //--Código terminado usando un useEffect antes de mejoras--
+    /*
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            .then(res => res.json())
+            .then(data => {
+                const { fact } = data
+                setFact(data.fact)
+
+                const firstWord = fact.split(" ")[0]
+                const threeWords = fact.split(" ").splice(0, 3).join(" ")
+                //las tres palabras se pasan en el fetch a la api pero la del ejercicio no funciona,
+                //por lo que se va a usar una api que devulve imágenes aleatorias
+
+                fetch(CAT_ENDPOINT_IMAGE_URL)
+                    .then(res => res.json())
+                    .then(response => {
+                        const { url } = response[0]
+                        setImageURL(url)
+                    })
+            }
+            )
+    }, [])//solo se ejecuta el useEffect la primera vez que se monta el componente
+*/
+    //--Mejoras al código usando dos useEffect--
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            .then(res => {
+                // TODO : handle error if !res.ok
+                if (!res.ok) {
+                    setFactError("No se ha podido recuperar la cita")
+                }
+                return res.json()
+            })
+            .then(data => {
+                const { fact } = data
+                setFact(data.fact)
+            })
+            .catch((err) => {
+                //tanto si hay un error con la respuesta 
+                //como si hay un error con la petición
+                console.log(err)
+            })
+    }, [])
+    //para recuperar la imagen cada vez que tenemos una cita nueva
+    useEffect(() => {
+        if (!fact) return
+
+        const firstWord = fact.split(" ")[0]
+        const threeWords = fact.split(" ").splice(0, 3).join(" ")
+        //las tres palabras se pasan en el fetch a la api pero la del ejercicio no funciona,
+        //por lo que se va a usar una api que devulve imágenes aleatorias
+
+        fetch(CAT_ENDPOINT_IMAGE_URL)
+            .then(res => res.json())
+            .then(response => {
+                const { url } = response[0]
+                setImageURL(url)
+            })
+    }, [fact])
+
+    return (
+        <main /*style={{
+            display: 'flex', alignItems: 'center', flexDirection: 'column'
+        }}*/>
+            <h1>Random Cat Fact</h1>
+
+            {fact && <p>{fact}</p>/*renderizado condicional*/}
+            {imageURL && <img src={imageURL} alt={`Image extracted using the first three words for ${fact}`} />}
+
+        </main >
+    )
+}
